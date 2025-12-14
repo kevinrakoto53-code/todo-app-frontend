980c5928ac12f5462c5e7579e4afea883f9afc0c
Narcis <kevinrakoto53@gmail.com>
2025-12-14 13:30:29 +0300

    update a api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://mon-api-express.onrender.com/api';
+const API_URL = 'https://todo-api-backend-h9ge.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
